988015e6bbe7124e12f6a263c8e3a3ec86e9722a
Timur Mishiev <timurmishiev@gmail.com>
2020-01-08 21:00:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   count = 0
     while count < array.length do 
-     if array.index(value_to_find) 
+     if array.[count} == value_to_find
+end     
     count += 1
 end
     nil 
